69880db944004ae9dd32ef427d04d31e3faced1c
Ron Hay <rhay@genomenon.com>
2020-03-12 16:21:52 -0400

    Added no-update option to self.sh

--- self.sh
+++ self.sh
@@ -5,6 +5,17 @@
 
 WRAPPER_VERSION="v0.1.0"
 
+has_param() {
+    local term="$1"
+    shift
+    for arg; do
+        if [[ $arg == "$term" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
 docker_error(){
     echo "ERROR: docker is not installed, or not in your PATH"
     exit 1
@@ -19,7 +30,7 @@ check_and_update_wrapper(){
         id=$(docker create heyronhay/self:latest); docker cp $id:/tmp/myapp/self.sh $fullpath; docker rm -v $id > /dev/null
 
         echo "updated and using new version!"
-        exec /bin/bash $fullpath $@
+        exec /bin/bash $fullpath --no-update $@
     else
         echo "up to date!"
     fi
@@ -28,6 +39,14 @@ check_and_update_wrapper(){
 # Check for docker on the system
 which docker > /dev/null 2>&1 || docker_error
 
-check_and_update_wrapper
+if ! has_param '--no-update' "$@"; then
+    check_and_update_wrapper
+fi
+
+for arg do
+    shift
+    [ "$arg" = "--no-update" ] && continue
+    set -- "$@" "$arg"
+done
 
 docker run -it --rm --user $(id -u):$(id -g) -v "$PWD":/workdir -w /workdir heyronhay/self:latest self $@
