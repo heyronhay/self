481532cb579feeabd2edc49c69e48498595731a2
Ron Hay <rhay@genomenon.com>
2020-03-15 13:38:05 -0400

    One other fix for self.bat

--- self.bat
+++ self.bat
@@ -1,6 +1,7 @@
 :: Windows batch file wrapper for a docker-based application.  Make sure this script is executable
 :: and put it in your PATH.
 @echo off
+
 docker pull heyronhay/self:latest > pull_output.txt
 findstr /m "up to date" pull_output.txt
 if NOT %errorlevel%==0 (
@@ -9,6 +10,7 @@ docker create heyronhay/self:latest > docker_id.txt
 set /p ID=<docker_id.txt
 docker cp %ID%:/tmp/myapp/self.bat temp.bat
 docker rm -v %ID%
-cmd /c copy /Y "temp.bat" "%~f0" & %~f0
+copy /Y "temp.bat" "%~f0" & %~f0
 )
+
 docker run -it --rm -v %CD%:/workdir -w /workdir heyronhay/self:latest self %*
